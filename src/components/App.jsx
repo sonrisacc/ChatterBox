1e0c137dc5bef559ac209f58441ed8198fc0ec75
Sonrisa <sonrisa722611@gmail.com>
2018-02-24 21:54:18 -0800

    Code clean up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,13 +4,11 @@ import socket from '../socketConnection';
 
 class App extends Component {
   render() {
-    console.log('running');
-    console.log('runnin222g');
     socket.on('news', data => {
       console.log(data);
       socket.emit('myTestEvent', { my: 'data' });
     });
-    return <div>Helloasadasdasdasd</div>;
+    return <div>Hello</div>;
   }
 }
 
