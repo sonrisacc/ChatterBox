90053c325fc8cc5a0bb5bb55f2e69cb4a9c5a1ba
Sonrisa <sonrisa722611@gmail.com>
2018-02-24 18:52:36 -0800

    Update config file and folder init

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  context: __dirname,
+  entry: './src/index.jsx',
+  devtool: 'cheap-eval-source-map',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public/'
+  },
+  devServer: {
+    hot: true,
+    publicPath: '/public/',
+    historyApiFallback: true,
+    port: 3001
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.css', '.scss']
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: true
+  },
+  // augmenting abilities of webpack
+  // will send down module name for debugging purposes
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.jsx?$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  }
+};
