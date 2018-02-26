c54b64dc8fb8ef19e3a17aee64388f8929ee2382
Sonrisa <sonrisa722611@gmail.com>
2018-02-25 20:50:54 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('../db/dbConnection');
 const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
@@ -18,12 +19,12 @@ const PORT = process.env.PORT || 8080;
 const HOST = 'localhost';
 const server = http.createServer(app);
 
+// static file server
 server.listen(PORT, () => {
   console.log(`listening on http://${HOST}:${PORT}`);
 });
 
+// socketio server
 const io = socketio().listen(server);
-
 app.io = io;
-
 chatSockets(io);
--- src/socketConnection.js
+++ src/socketConnection.js
@@ -1,6 +1,5 @@
 const io = require('socket.io-client');
 
-console.log('client', process.env.HEROKU_URL);
 const url = process.env.HEROKU_URL || 'http://localhost:3033/';
 
 const socket = io.connect(url);
