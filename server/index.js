f7d22d5ebbfcbadfd1886594d41afd9591edf42a
Sonrisa <sonrisa722611@gmail.com>
2018-02-25 20:22:45 -0800

    Test remove socket.io from server

--- server/index.js
+++ server/index.js
@@ -1,20 +1,20 @@
 require('dotenv').config();
 require('../db/dbConnection');
-const chatSockets = require('./sockets/chat-socket.js');
-// const http = require('http');
+// const chatSockets = require('./sockets/chat-socket.js');
+const http = require('http');
 const app = require('./server.js');
-const socketio = require('socket.io');
+// const socketio = require('socket.io');
 
 console.info('process.env.NODE_ENV', process.env.NODE_ENV);
 const PORT = process.env.PORT || 3033;
 
-// const server = http.createServer(app);
-const server = app.listen(PORT, 'localhost', () =>
+const server = http.createServer(app);
+server.listen(PORT, 'localhost', () =>
   console.log('http server listening on PORT: ', PORT)
 );
 //
-const io = socketio().listen(server);
-
-app.io = io;
-
-chatSockets(io);
+// const io = socketio().listen(server);
+//
+// app.io = io;
+//
+// chatSockets(io);
