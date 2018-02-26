91923b194612285e9517c8bb31fc31e6add2665d
Sonrisa <sonrisa722611@gmail.com>
2018-02-25 20:25:26 -0800

    Test for heroku

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
 require('dotenv').config();
 require('../db/dbConnection');
 // const chatSockets = require('./sockets/chat-socket.js');
-const http = require('http');
+// const http = require('http');
 const app = require('./server.js');
 // const socketio = require('socket.io');
 
 console.info('process.env.NODE_ENV', process.env.NODE_ENV);
+
 const PORT = process.env.PORT || 3033;
 
-const server = http.createServer(app);
-server.listen(PORT, 'localhost', () =>
+// const server = http.createServer(app);
+app.listen(PORT, 'localhost', () =>
   console.log('http server listening on PORT: ', PORT)
 );
 //
