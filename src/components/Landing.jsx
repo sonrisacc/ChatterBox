7e9827dd9da7308637c783731bec1238c32d1ca7
Sonrisa <sonrisa722611@gmail.com>
2018-02-26 16:33:10 -0800

    Init redux with stateful component

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,42 +1,51 @@
-// import React, { Component } from 'react';
-import React from 'react';
+import React, { Component } from 'react';
+// import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { setLoginUserName } from '../actions/actionCreators';
 
-const Landing = props => (
-  // state = {
-  //   userNameInput: ''
-  // };
+class Landing extends Component {
+  state = {
+    userNameInput: ''
+  };
 
   // handleSubmit = event => {
   //   console.log('searched', props.loginUsername);
   //   event.preventDefault();
   // };
   //
-  // handleChange = event => {
-  //   this.setState({ userNameInput: event.target.value });
-  // };
+  handleChange = event => {
+    this.setState({ userNameInput: event.target.value });
+  };
+  render() {
+    return (
+      <div className="landing">
+        <h1>Hello {this.props.loginUsername}</h1>
 
-  <div className="landing">
-    <h1>Hello {props.loginUsername}</h1>
-
-    <input
-      type="text"
-      placeholder="Input username here"
-      onChange={props.handleLoginUserNameChange}
-    />
-    {/* <button onClick={this.handleSubmit}>LogIn</button> */}
-    <button>
-      <Link to="/signUp"> SignUp </Link>
-    </button>
-  </div>
-);
+        <input
+          type="text"
+          placeholder="Input username here"
+          onChange={this.handleChange}
+        />
+        <button
+          onClick={() =>
+            this.props.handleLoginUserNameChange(this.state.userNameInput)
+          }
+        >
+          LogIn
+        </button>
+        <button>
+          <Link to="/signUp"> SignUp </Link>
+        </button>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = state => ({ loginUsername: state.loginUsername });
 const mapDispatchToProps = dispatch => ({
-  handleLoginUserNameChange(event) {
-    dispatch(setLoginUserName(event.target.value));
+  handleLoginUserNameChange(username) {
+    dispatch(setLoginUserName(username));
   }
 });
 
