7956ecdb4205f9c26095df55b745f1ce5c6da256
Sonrisa <sonrisa722611@gmail.com>
2018-02-27 15:32:03 -0800

    Update config heroku url

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let HEROKU_URL = JSON.stringify('http://localhost:8080');
 if (process.env.NODE_ENV === 'production') {
-  HEROKU_URL = JSON.stringify('https://shielded-bastion-15522.herokuapp.com/');
+  HEROKU_URL = JSON.stringify('https://shielded-bastion-15522.herokuapp.com');
 }
 
 module.exports = HEROKU_URL;
