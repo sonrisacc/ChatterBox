be0190b5b9a0f6c7c1085501bf599ef04c7eb848
Sonrisa <sonrisa722611@gmail.com>
2018-02-28 13:08:25 -0800

    Test auto sizing input field

--- src/components/Inputbox.jsx
+++ src/components/Inputbox.jsx
@@ -2,6 +2,21 @@ import React, { Component } from 'react';
 // import { connect } from 'react-redux';
 
 class Inputbox extends Component {
+  // state = {
+  //   msgBody: ''
+  // };
+
+  setRef = node => {
+    this.test = node;
+  };
+  handleKeyPress = e => {
+    if (e.charCode === 13 && e.altKey) {
+      console.log('do validate');
+      console.log('hey', this.test.value);
+      this.test.value = '';
+    }
+  };
+
   render() {
     return (
       <div className="input-box">
@@ -13,18 +28,27 @@ class Inputbox extends Component {
             <ul>5 min</ul>
             <ul>1 hr</ul>
           </div>
-          <button className="dropbtn">Dropdown</button>
+          <button className="dropbtn">+</button>
         </div>
-        <div
+        {/* <div role="button" onKeyPress={this.handleKeyPress}> */}
+        {/* <div
+            className="input-area"
+            contentEditable="true"
+            aria-multiline="true"
+            role="textbox"
+            aria-label="Note"
+            spellCheck="true"
+
+          /> */}
+        <textarea
           className="input-area"
-          contentEditable="true"
-          aria-multiline="true"
-          role="textbox"
-          aria-label="Note"
-          spellCheck="true"
-        >
-          Start typing here..
-        </div>
+          rows="4"
+          cols="50"
+          ref={this.setRef}
+          placeholder="Type......."
+          onKeyPress={this.handleKeyPress}
+        />
+        {/* </div> */}
       </div>
     );
   }
